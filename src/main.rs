bd1e8a6275ccf24052d7758a67629218556f45b9
Patrick <patches72790@gmail.com>
2022-08-06 23:32:16 -0500

    added code for part 2 of blog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // don't link rust standard library
+#![no_main] // disable all rust level entry points (i.e. main)
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+static HELLO: &[u8] = b"Hello World!";
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte; // char byte
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // cyan color
+        }
+    }
+
+    loop {}
 }
